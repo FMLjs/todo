0eee8da3b6fe7fb0b749fe84a1be1307d255618d
FMLjs <mrcofest@gmail.com>
2020-10-29 18:24:12 +0200

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, () => console.log('Listening at 9000'));
+app.listen(PORT, () => console.log('Listening at ', PORT));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.redirect('/index.html'));
+app.get('/', (req, res) => res.redirect('index.html'));
