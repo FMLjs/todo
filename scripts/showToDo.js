1b8c15e500e4b62d63ff88d44546077ad4879669
FMLjs <mrcofest@gmail.com>
2020-11-04 18:57:02 +0200

    make it better

--- scripts/showToDo.js
+++ scripts/showToDo.js
@@ -138,7 +138,7 @@ function addToPage(toDoOb) {
     btnEdt.style.color = 'green';
     btnEdt.addEventListener('click', () => {
         localStorage.setItem('edit', JSON.stringify(toDoOb));
-        alert(window.location.href)
+        alert("aue", window.location.href)
         window.location.href = "/pages/toDoCreation.html";
     });
     let btns = document.createElement('div');
